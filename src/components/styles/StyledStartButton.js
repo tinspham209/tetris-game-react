24cc4f9f49e94b5fefa918ad53a7cf77a7f18015
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-03 23:22:00 +0700

    Styling for StartButton

---
+++ src/components/styles/StyledStartButton.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const StyledStartButton = styled.button`
+	min-height: 30px;
+	width: 100%;
+	box-sizing: border-box;
+
+	margin: 0 0 20px 0;
+	padding: 20px;
+
+	border-radius: 20px;
+	border: none;
+	outline: none;
+
+	color: white;
+	background: #333;
+
+	font-size: 0.9rem;
+	font-family: Pixel, Arial, Helvetica, sans-serif;
+
+	cursor: pointer;
+`;
