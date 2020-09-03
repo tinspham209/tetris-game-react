2d4525d9e56b42ff440db6f3de08af049cdbaac6
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-03 23:09:57 +0700

    import StyledTetris to Tetris component

--- src/components/Tetris/index.jsx
+++ src/components/Tetris/index.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import Stage from "../Stage";
 import Display from "../Display";
 import StartButton from "../StartButton";
+import { createState } from "../../gameHelper";
+import { StyledTetrisWrapper, StyledTetris } from "../styles/StyledTetris";
 
 const Tetris = () => {
 	return (
-		<div>
-			<Stage />
-			<aside>
-				<div>
-					<Display text="Score" />
-					<Display text="Rows" />
-					<Display text="Level" />
-				</div>
-				<StartButton />
-			</aside>
-		</div>
+		<StyledTetrisWrapper>
+			<StyledTetris>
+				<Stage state={createState()} />
+				<aside>
+					<div>
+						<Display text="Score" />
+						<Display text="Rows" />
+						<Display text="Level" />
+					</div>
+					<StartButton />
+				</aside>
+			</StyledTetris>
+		</StyledTetrisWrapper>
 	);
 };
 
