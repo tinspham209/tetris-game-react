75157824fadba2537eba15828c1b21b1010cff67
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-03 23:21:40 +0700

    Styling for Display

---
+++ src/components/styles/StyledDisplay.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const StyledDisplay = styled.div`
+	box-sizing: border-box;
+	min-height: 30px;
+	width: 100%;
+
+	display: flex;
+	align-items: center;
+
+	margin: 0 0 20px 0;
+	padding: 20px;
+
+	border: 4px solid #333;
+	border-radius: 20px;
+
+	color: ${(props) => (props.gameOver ? "red" : "#999")};
+	background: #000;
+
+	font-family: Pixel, Arial, Helvetica, sans-serif;
+	font-size: 0.8rem;
+`;
