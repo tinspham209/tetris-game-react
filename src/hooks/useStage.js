3f3fc4e09d4c16395a66c501cae45de61facc77d
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-05 00:08:41 +0700

    check collided

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 
 import { createStage } from "../gameHelper";
-import Cell from "../components/Cell";
 
 export const useStage = (player, resetPlayer) => {
 	const [stage, setStage] = useState(createStage());
@@ -10,7 +9,7 @@ export const useStage = (player, resetPlayer) => {
 		const updateStage = (prevStage) => {
 			// First flush the stage
 			const newStage = prevStage.map((row) =>
-				row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : Cell))
+				row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : cell))
 			);
 			// Then draw the tetromino
 			player.tetromino.forEach((row, y) => {
