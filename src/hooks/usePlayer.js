4f5d5ec24280be5b453c960c45b984d94ab1b53a
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-05 00:22:42 +0700

    check when rotate tetromino in collied

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,7 +1,7 @@
 import { useState, useCallback } from "react";
 
 import { TETROMINOS, randomTetromino } from "../tetrominos";
-import { STAGE_WIDTH } from "../gameHelper";
+import { STAGE_WIDTH, checkCollision } from "../gameHelper";
 
 export const usePlayer = () => {
 	const [player, setPlayer] = useState({
@@ -29,6 +29,17 @@ export const usePlayer = () => {
 		const clonedPlayer = JSON.parse(JSON.stringify(player));
 		clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
 
+		const pos = clonedPlayer.pos.x;
+		let offset = 1;
+		while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+			clonedPlayer.pos.x += offset;
+			offset = -(offset + (offset > 0 ? 1 : -1));
+			if (offset > clonedPlayer.tetromino[0].length) {
+				rotate(clonedPlayer.tetromino, -dir);
+				clonedPlayer.pos.x = pos;
+				return;
+			}
+		}
 		setPlayer(clonedPlayer);
 	};
 
