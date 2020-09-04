4d709d9587a5b51afb01829585364d203086e9a7
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-04 22:35:17 +0700

    updatedPlayerPos

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 import { randomTetromino } from "../tetrominos";
+import { STAGE_WIDTH } from "../gameHelper";
 
 export const usePlayer = () => {
 	const [player, setPlayer] = useState({
@@ -11,5 +12,17 @@ export const usePlayer = () => {
 		tetromino: randomTetromino().shape,
 		collided: false,
 	});
-	return [player];
+
+	const updatePlayerPos = ({ x, y, collided }) => {
+		setPlayer((prev) => ({
+			...prev,
+			pos: {
+				x: (prev.pos.x += x),
+				y: (prev.pos.y += y),
+				collided,
+			},
+		}));
+	};
+
+	return [player, updatePlayerPos];
 };
