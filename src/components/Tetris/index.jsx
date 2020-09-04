ef9fb29a6bd86f05700373b7b4d01ba49854b969
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-04 23:04:47 +0700

    fix wrong code spelling

--- src/components/Tetris/index.jsx
+++ src/components/Tetris/index.jsx
@@ -8,16 +8,54 @@ import { StyledTetrisWrapper, StyledTetris } from "../styles/StyledTetris";
 import { usePlayer } from "../../hooks/usePlayer";
 import { useStage } from "../../hooks/useStage";
 
+import { createStage } from "../../gameHelper";
+
 const Tetris = () => {
 	const [dropTime, setDropTime] = useState(null);
 	const [gameOver, setGameOver] = useState(false);
 
-	const [player] = usePlayer();
+	const [player, updatePlayerPos, resetPlayer] = usePlayer();
 	const [stage, setStage] = useStage(player);
 
 	console.log("re-render");
+
+	const movePlayer = (dir) => {
+		updatePlayerPos({
+			x: dir,
+			y: 0,
+		});
+	};
+
+	const startGame = () => {
+		setStage(createStage());
+		resetPlayer();
+	};
+
+	const drop = () => {
+		updatePlayerPos({ x: 0, y: 1 });
+	};
+
+	const dropPlayer = () => {
+		drop();
+	};
+
+	const move = ({ keyCode }) => {
+		if (!gameOver) {
+			if (keyCode === 37) {
+				//Left
+				movePlayer(-1);
+			} else if (keyCode === 39) {
+				//Right
+				movePlayer(1);
+			} else if (keyCode === 40) {
+				// Down
+				dropPlayer();
+			}
+		}
+	};
+
 	return (
-		<StyledTetrisWrapper>
+		<StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={(e) => move(e)}>
 			<StyledTetris>
 				<Stage state={stage} />
 				<aside>
@@ -30,7 +68,7 @@ const Tetris = () => {
 							<Display text="Level" />
 						</div>
 					)}
-					<StartButton />
+					<StartButton callback={startGame} />
 				</aside>
 			</StyledTetris>
 		</StyledTetrisWrapper>
